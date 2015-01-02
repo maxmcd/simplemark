8ad9a99ea18bc756614ce39e222a70f54d3a147b
maxmcd <max.t.mcdonnell@gmail.com>
2015-01-01 20:16:57 -0500

    safer markdown rendering

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,1 +1,7 @@
-$markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+$markdown = Redcarpet::Markdown.new(
+    Redcarpet::Render::HTML, 
+    autolink: true, 
+    tables: true,
+    escape_html: true,
+    safe_links_only: true,
+    with_toc_data: true)
